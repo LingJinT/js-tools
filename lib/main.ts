23ab79a3d711d50642f4b231fb5f73c5d2a31fe7
l2015 <lingjt@dtdream.com>
2023-02-27 15:14:41 +0800

    feat: debounce

--- lib/main.ts
+++ lib/main.ts
@@ -1,9 +1,25 @@
+export interface DebounceFn<T extends (...args: any[]) => any> {
+  (...args: Parameters<T>): ReturnType<T> | void
+}
+
+export function debounce<T extends (...args: any[]) => any>(fn: T, delay: number): DebounceFn<T> {
+  let timer: number | undefined
+  return (...args) => {
+    if(timer !== undefined) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn(...args)
+      timer = undefined
+    }, delay)
+  }
+}
+
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(++counter))
-  setCounter(0)
+  const setCounter = debounce(() => {
+    element.innerHTML = `count is ${counter++}`
+  }, 300)
+  element.addEventListener('click', () => setCounter())
+  setCounter()
 }
