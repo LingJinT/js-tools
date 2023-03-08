2ccac97ed4f24ff39c0e6acc80a50b72e7c2b004
l2015 <lingjt@dtdream.com>
2023-03-08 18:08:14 +0800

    feat: eHub

--- lib/eHub.ts
+++ lib/eHub.ts
@@ -1,14 +1,30 @@
-function eHub<E>(all?: Map<keyof E, E[keyof E]>) {
+export type Callback<T> = (e: T) => void
+
+function eHub<E extends Record<string | symbol, unknown>>(all?: Map<keyof E, Array<Callback<E[keyof E]>>>) {
   all = all || new Map()
   return {
     all,
-    on(key, callback) {
-      all!.set(key, callback)
+    on<K extends keyof E>(key: K, callback: Callback<E[keyof E]>) {
+      let callbacks = all!.get(key)
+      if(callbacks) {
+        callbacks.push(callback)
+      }
+      callbacks = [callback]
+      all!.set(key, callbacks)
     },
-    off(key) {
-
+    off<K extends keyof E>(key: K, callback: Callback<E[keyof E]>) {
+      const callbacks = all!.get(key)
+      if(callbacks && callback) {
+        callbacks.splice(callbacks.indexOf(callback), 1)
+      } else {
+        all!.set(key, [])
+      }
     },
-    emit(key, value) {
+    emit<K extends keyof E>(key: K, value?: E[K]) {
+      const callbacks = (all!.get(key) || []).concat(all!.get('*') || [])
+      callbacks?.forEach(fn => {
+        fn(value!)
+      })
 
     }
   }
